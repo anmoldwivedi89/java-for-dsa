8f2ad3660d36ccfda51d9b0f9cd6d6866f418344
Anmol <anmoldwivedi2002@outlook.com>
2026-09-07 20:16:15 +0530

    Armstrong problem

--- java practice/Gcd.java
+++ java practice/Gcd.java
@@ -1,6 +1,8 @@
 class Gcd {
     public static void main(String[] args) {
        System.out.println("GCD of 12 and 15 is: " + gcd(12, 15));
+       System.out.println("LCM of 12 and 15 is: " + lcm(12, 15));
+       
     }
    static int gcd(int a, int b) {
       while(b!=0){
@@ -9,4 +11,11 @@ class Gcd {
         a = oldValueOfb;
       }
       return a;
-    }}
+    }
+    static int lcm(int a, int b){
+        int gcd = gcd(a,b);
+        int product = a*b;
+        int lcm = product/gcd;
+        return lcm;
+    }
+}
